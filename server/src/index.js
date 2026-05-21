444e6e98aca6b290e7c9532a3ce729276b7f3877
CodecAnuj <anujtiwari9111@gmail.com>
2026-05-21 13:49:57 +0000

    add server entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,20 @@
 // this is the entry point of our server , server will starts from here
 
 import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./db/index.js";
+const app = express();
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
