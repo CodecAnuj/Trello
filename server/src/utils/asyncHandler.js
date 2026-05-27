5340a7491501c702be1823dc6b2fd4c59a1bb07d
CodecAnuj <anujtiwari9111@gmail.com>
2026-05-27 17:08:27 +0000

    add utils code

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// const asyncHandler = async (requestHandler) => {
+//     (req , res , next) => {
+//         Promise.resolve(requestHandler(req , res , next)).catch((err) => {
+//             next(err);
+//         })
+//     }
+// }
+
+// currently using this one , as this feels easy now ......
+
+const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message || "internal server error",
+    });
+  }
+};
+
+export { asyncHandler };
